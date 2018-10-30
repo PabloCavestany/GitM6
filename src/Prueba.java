a750c628af37bbd2c08cf61b47cbe180e18e6716
pablocavestany <pablo.cavestany@gmail.com>
2018-10-31 00:49:00 +0100

    jjj

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,4 +1,8 @@
 
 public class Prueba {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Helllo Wrld");
+      System.out.println("pruebas");
+	}
 }
